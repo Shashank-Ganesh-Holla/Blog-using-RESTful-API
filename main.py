00fdba25bd6796704a833965db91d152c6a54818
Shashank-Ganesh-Holla <suyogshashank@gmail.com>
2023-06-24 18:41:19 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,131 @@
+from datetime import date
+
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+
+import os
+
+db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'posts.db')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Set your secret key'
+ckeditor = CKEditor(app)
+# app.config['CKEDITOR_PKG_TYPE'] = 'basic'
+Bootstrap(app)
+
+##CONNECT TO DB
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+@app.route('/')
+def get_all_posts():
+    if not os.path.exists('posts.db'):
+        db.create_all()
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    posts = BlogPost.query.get(index)
+    return render_template("post.html", post=posts)
+
+
+@app.route('/new-post', methods=['GET', 'POST'])
+def new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=form.author.data,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form)
+
+
+@app.route("/edit/<int:post_id>", methods=['GET', 'POST'])
+def edit_post(post_id):
+    id_post = post_id
+    post = BlogPost.query.get(id_post)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        try:
+            post.title = edit_form.title.data
+            post.subtitle = edit_form.subtitle.data
+            post.body = edit_form.body.data
+            post.img_url = edit_form.img_url.data
+            post.author = edit_form.author.data
+            db.session.commit()
+            return redirect(url_for('show_post', index=id_post))
+        except Exception as err:
+            print(repr(err))
+
+    return render_template("make-post.html", form=edit_form, identity=True)
+
+
+@app.route('/delete/<int:post_id>')
+def post_delete(post_id):
+    delete_id = post_id
+
+    try:
+        post_to_delete = BlogPost.query.get(delete_id)
+
+        db.session.delete(post_to_delete)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+    except Exception as err:
+        print(repr(err))
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
